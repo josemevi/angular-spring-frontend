8b03499f2aacd17f9068bae40b598d006e70b790
Jose Melian <josedavidmelian@gmail.com>
2021-04-23 12:41:41 +0100

    creating and implemeting the ep to get products from the db·

--- src/app/facturas/facturas.component.ts
+++ src/app/facturas/facturas.component.ts
@@ -4,7 +4,11 @@ import { ClientesService } from "../clientes/clientes.service";
 import { ActivatedRoute } from '@angular/router';
 import {FormControl} from '@angular/forms';
 import {Observable} from 'rxjs';
-import {map, startWith} from 'rxjs/operators';
+//import {filter, map, startWith } from 'rxjs/operators';
+import { map,flatMap } from "rxjs/operators";
+
+import { FacturaService } from "./services/factura.service";
+import { Producto } from './models/producto';
 
 
 @Component({
@@ -18,18 +22,22 @@ export class FacturasComponent implements OnInit {
   factura: Factura = new Factura();
 
   autoCompletecontrol = new FormControl();
-  productos: string[] = ['One', 'Two', 'Three'];
-  productosFiltrados: Observable<string[]>;
+  //productos: string[] = ['One', 'Two', 'Three'];
+  //productosFiltrados: Observable<string[]>;
+  productosFiltrados: Observable<Producto[]>;
 
   constructor(private clienteService: ClientesService,
-    private activatedRoute: ActivatedRoute) { }
+    private activatedRoute: ActivatedRoute,
+    private facturaService: FacturaService) { }
 
   ngOnInit(): void {
 
     this.productosFiltrados = this.autoCompletecontrol.valueChanges
     .pipe(
-      startWith(''),
-      map(value => this._filter(value))
+      //startWith(''),
+      //map(value => this._filter(value))
+      map(value => typeof value === 'string'? value : value.nombre),
+      flatMap(value => value ?  this._filter(value) : [])
     );
 
     this.activatedRoute.paramMap.subscribe(params => {
@@ -41,10 +49,16 @@ export class FacturasComponent implements OnInit {
   }
 
 
-  private _filter(value: string): string[] {
+  private _filter(value: string): Observable<Producto[]>{
     const filterValue = value.toLowerCase();
 
-    return this.productos.filter(option => option.toLowerCase().includes(filterValue));
+    //return this.productos.filter(option => option.toLowerCase().includes(filterValue));
+
+    return this.facturaService.filtrarProductos(filterValue);
+  }
+
+  showName(producto?: Producto): string | undefined{
+    return producto? producto.nombre : undefined;
   }
 
 }
