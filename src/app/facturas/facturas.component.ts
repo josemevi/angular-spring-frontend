42bd54b130e590b9d7729a56398ec0f7e8f1358a
Jose Melian <josedavidmelian@gmail.com>
2021-04-23 14:31:43 +0100

    adding factura items and validating the amount of every item

--- src/app/facturas/facturas.component.ts
+++ src/app/facturas/facturas.component.ts
@@ -9,6 +9,8 @@ import { map,flatMap } from "rxjs/operators";
 
 import { FacturaService } from "./services/factura.service";
 import { Producto } from './models/producto';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { ItemFactura } from './models/item-factura';
 
 
 @Component({
@@ -21,7 +23,7 @@ export class FacturasComponent implements OnInit {
   title: string = "Nueva Factura";
   factura: Factura = new Factura();
 
-  autoCompletecontrol = new FormControl();
+  autoCompleteControl = new FormControl();
   //productos: string[] = ['One', 'Two', 'Three'];
   //productosFiltrados: Observable<string[]>;
   productosFiltrados: Observable<Producto[]>;
@@ -32,7 +34,7 @@ export class FacturasComponent implements OnInit {
 
   ngOnInit(): void {
 
-    this.productosFiltrados = this.autoCompletecontrol.valueChanges
+    this.productosFiltrados = this.autoCompleteControl.valueChanges
     .pipe(
       //startWith(''),
       //map(value => this._filter(value))
@@ -59,6 +61,54 @@ export class FacturasComponent implements OnInit {
 
   showName(producto?: Producto): string | undefined{
     return producto? producto.nombre : undefined;
+  }
+
+  selectProducto(event: MatAutocompleteSelectedEvent): void {
+    let producto = event.option.value as Producto;
+
+    if(this.alreadyInList(producto.id)){
+      this.incrementInList(producto.id)
+    }else {
+      let newItem = new ItemFactura();
+      newItem.producto = producto;
+      this.factura.items.push(newItem); 
+    }
+
+    this.autoCompleteControl.setValue('');
+    event.option.focus();
+    event.option.deselect();
+  }
+
+  actualizarCantidad(id: number, event: any): void{
+    let cantidad: number = event.target.value as number;
+
+    this.factura.items = this.factura.items.map((item:ItemFactura) => {
+      if(id === item.producto.id){
+        item.cantidad = cantidad;
+      }
+      return item;
+    })
+  }
+
+  alreadyInList(id:number): boolean {
+    let exist = false;
+    this.factura.items.forEach((item: ItemFactura) => {
+      if(id === item.producto.id){
+        exist = true;
+      }
+    })
+    return exist;
+  }
+
+  incrementInList(id:number): void {
+
+    this.factura.items = this.factura.items.map((item:ItemFactura) => {
+      if(id === item.producto.id){
+        ++item.cantidad;
+      }
+      return item;
+    })
+    
   }
 
 }
